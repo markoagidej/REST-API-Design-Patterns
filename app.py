2e4d0f6aff842a4f340008c4ee187a786d9c4e48
Marko <markogidej@yahoo.com>
2024-08-22 11:53:05 -0400

    Tas k3 done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from database import db
 from schema import ma
+from limiter import limiter
 
 from models.customer import Customer
 from models.employee import Employee
@@ -20,6 +21,7 @@ def create_app(config_name):
     app.config.from_object(f'config.{config_name}')
     db.init_app(app)
     ma.init_app(app)
+    limiter.init_app(app)
 
     return app
 
@@ -29,6 +31,13 @@ def blueprint_config(app):
     app.register_blueprint(order_blueprint, url_prefix='/orders')
     app.register_blueprint(product_blueprint, url_prefix='/products')
     app.register_blueprint(production_blueprint, url_prefix='/productions')
+
+def configure_rate_limit():
+    limiter.limit("10/day")(customer_blueprint)
+    limiter.limit("10/day")(employee_blueprint)
+    limiter.limit("10/day")(order_blueprint)
+    limiter.limit("10/day")(product_blueprint)
+    limiter.limit("10/day")(production_blueprint)
 
 if __name__ == '__main__':
     app = create_app('DevelopmentConfig')
